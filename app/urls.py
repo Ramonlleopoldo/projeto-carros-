d2a108331a3c0f95d24067838f8927115143a801
Ramon Leopoldo <ramon.jl@outlook.com>
2024-09-04 19:30:59 -0300

    criação de filtro atraves do search, ajuste no html e css, alteração no nome da função da request no arquivo Cars/views

--- app/urls.py
+++ app/urls.py
@@ -18,10 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import MostrarCarro
+from cars.views import Cars_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cars/',MostrarCarro ),
+    path('cars/',Cars_views,name= 'cars_list' ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from cars.models import Car
 
-def MostrarCarro(request):
-  cars = Car.objects.filter(brand__name = 'Volkswagen')
+def Cars_views(request):
+  cars = Car.objects.all().order_by('model')
+  search = request.GET.get('search')
+  if search:
+    cars = Car.objects.filter(model__icontains = search)
   return render(request, 'cars.html',{'cars':cars } )
 
  
