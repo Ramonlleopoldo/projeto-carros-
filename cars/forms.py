fdf27c7e2b54b3a12a9d43bf9734ae37a68d3890
Ramon Leopoldo <ramon.jl@outlook.com>
2024-09-10 11:31:13 -0300

    Ajuste de forulario, criado a funçao de cadastrar veiculos.

--- cars/forms.py
+++ cars/forms.py
@@ -1,5 +1,5 @@
 from django import forms   #importação a classe 'forms'que contem varias funções ja criadas do django sobre o forms.
-from cars.models import Brand # Devemos importar o models de brand para que possamos usar o orm capturando todos os modelos cadastrados
+from cars.models import Brand, Car# Devemos importar o models de brand para que possamos usar o orm capturando todos os modelos cadastrados
 #Criamos uma classe que contem todos inputs que queremos que seja cadastrados, esses inputs devem ter relação com a tabela criada em models.
 class CarForm(forms.Form):
     model = forms.CharField(max_length=200)
@@ -9,3 +9,16 @@ class CarForm(forms.Form):
     plate = forms.CharField(max_length=10)
     value = forms.FloatField()
     photo = forms.ImageField()
+
+    def save(self):
+        car = Car(
+            model = self.cleaned_data['model'],
+            brand = self.cleaned_data['brand'],
+            factory_year = self.cleaned_data['factory_year'],
+            model_year =  self.cleaned_data['model_year'],
+            plate = self.cleaned_data['plate'],
+            photo = self.cleaned_data['photo'],
+        )
+        #este save é uma função do models(tabela do banco de dados) que vai salvar as informações acima no banco de dados
+        car.save()
+        return car
--- cars/views.py
+++ cars/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarForm
 
@@ -10,7 +10,15 @@ def cars_view(request):
   return render(request, 'cars.html',{'cars':cars } )
 
 def new_car_view(request):
-  new_car_form = CarForm() #Atribuindo a função criada no forms a uma variavel.
-  
-  return render(request,'new_car.html', {'new_car_form':new_car_form})
-  #           requisição, template que queremos renderizar, Contexto --> conseguimos usar essa variavel no html
+    if request.method == 'POST':
+        new_car_form = CarForm(request.POST, request.FILES)
+        #if de verificação se os campos preenchidos foram validados conforme validações escritas no forms.
+        if new_car_form.is_valid():
+           #caso seja validado vai salvar, o save é uma função que escrevemos no forms.py
+           new_car_form.save()
+           #o retur rediceriona o usuario para lista de carros
+           return redirect('cars_list')
+    else:
+        new_car_form = CarForm() #Atribuindo a função criada no forms a uma variavel.
+    return render(request,'new_car.html', {'new_car_form':new_car_form})
+    # requisição, template que queremos renderizar, Contexto --> conseguimos usar essa variavel no html
