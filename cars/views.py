89566f69adcf1d35aebfc5505dfb766bc52190ef
Ramon Leopoldo <ramon.jl@outlook.com>
2024-09-19 20:06:59 -0300

    Alteração de function based views para Class Based Views

--- cars/views.py
+++ cars/views.py
@@ -1,24 +1,31 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views import View
+from django.views.generic import ListView
 
-def cars_view(request):
-  cars = Car.objects.all().order_by('model')
-  search = request.GET.get('search')
-  if search:
-    cars = Car.objects.filter(model__icontains = search)
-  return render(request, 'cars.html',{'cars':cars } )
+class CarsView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
+    #sobrescrevendo o metodo get_queryset para aplicar filtros.
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
+        if search:
+            cars = Car.objects.filter(model__icontains = search)
+        return cars
 
-def new_car_view(request):
-    if request.method == 'POST':
+class NewCarView(View):
+    def get(self, request):
+        new_car_form = CarModelForm()
+        return render(request,'new_car.html', {'new_car_form':new_car_form})
+    def post(self,request):
         new_car_form = CarModelForm(request.POST, request.FILES)
-        #if de verificação se os campos preenchidos foram validados conforme validações escritas no forms.
         if new_car_form.is_valid():
-           #caso seja validado vai salvar, o save é uma função que escrevemos no forms.py
-           new_car_form.save()
-           #o retur rediceriona o usuario para lista de carros
-           return redirect('cars_list')
-    else:
-        new_car_form = CarModelForm() #Atribuindo a função criada no forms a uma variavel.
-    return render(request,'new_car.html', {'new_car_form':new_car_form})
-    # requisição, template que queremos renderizar, Contexto --> conseguimos usar essa variavel no html
+            new_car_form.save()
+            return redirect('cars_list')
+        return render(request,'new_car.html', {'new_car_form':new_car_form})
+    
