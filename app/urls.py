1166d724a63724d04ca4f6c6f2543f2f29c4e172
Ramon Leopoldo <ramon.jl@outlook.com>
2024-09-20 11:32:04 -0300

    Usando a view generica CreateView para realizar criação de novos carros.

--- app/urls.py
+++ app/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import CarsView, NewCarView
+from cars.views import CarsView, NewCarCreateView
 from accounts.views import register_view, login_view, logout_view
 
 urlpatterns = [
@@ -27,5 +27,5 @@ urlpatterns = [
     path('cars/',CarsView.as_view(), name= 'cars_list' ),
     path('login/', login_view, name='login'),
     path('logout/',logout_view, name='logout'),
-    path('new_car/',NewCarView.as_view(), name='new_car'),
+    path('new_car/',NewCarCreateView.as_view(), name='new_car'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
