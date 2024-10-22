53ca4e29c1d3978c1487c5c5b2ed4e67e71b8893
Ramon Leopoldo <ramon.jl@outlook.com>
2024-10-22 09:36:21 -0300

    ajustes de autenticação e botões de car_details

--- cars/views.py
+++ cars/views.py
@@ -2,9 +2,12 @@
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, CreateView, UpdateView,DeleteView
 from django.views.generic.detail import DetailView
 
+#LISTA DE CARROS
 class CarsView(ListView):
     model = Car
     template_name = 'cars.html'
@@ -16,16 +19,23 @@ class CarsView(ListView):
         if search:
             cars = Car.objects.filter(model__icontains = search)
         return cars
+    
+
+#INSERIR NOVOS CARROS
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class NewCarCreateView(CreateView):
     model = Car
     form_class = CarModelForm
     template_name = 'new_car.html'
     success_url = '/cars/'
 
+#DETALHES DOS CARROS
 class DetailCarView(DetailView):
     model = Car
     template_name = 'car_detail.html'
 
+#ATUALIZAR CARROS
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class UpdateCarView(UpdateView):
     model = Car
     form_class = CarModelForm
@@ -33,9 +43,9 @@ class UpdateCarView(UpdateView):
 
     def get_success_url(self):
         return reverse_lazy('detail_car', kwargs={'pk': self.object.pk})
-
     
-    
+#DELETAR CARROS
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class DeleteCarView(DeleteView):
     model = Car
     template_name = 'car_delete.html'
